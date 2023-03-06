0d5c08707ec3c051f36f229c4775adfd61c89f07
Andyk5creator <119440912+Andyk5creator@users.noreply.github.com>
2023-03-06 20:35:35 +0200

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-
-const burger = document.querySelector('.burger-icon');
-const menu = document.querySelector('.mobile-menu');
-
-burger.addEventListener('click', function () {
-	menu.classList.toggle('open');
-});
